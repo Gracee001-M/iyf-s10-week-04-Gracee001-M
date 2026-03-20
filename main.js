91e8940efd6ee187e9bb93e3b5e2e9717d624d90
Gracee001-M <gracemacharia031@gmail.com>
2026-03-20 18:40:32 +0000

    Add  JavaScript Setup & Variables

---
+++ main.js
@@ -0,0 +1,21 @@
+// Variable Practice
+
+// 1. Your name (string)
+let myName = "Grace Macharia";
+console.log("My name is: " + myName);
+
+// 2. Your age (number)
+let myAge = 19;
+console.log("My age is: " + myAge);
+
+// 3. Whether you're a student (boolean)
+let isStudent = true;
+console.log("Am I a student? " + isStudent);
+
+// 4. Your favorite colors (array)
+let favoriteColors = ["pink", "purple", "black"];
+console.log("My favorite colors are: " + favoriteColors);
+
+// 5. Today's date (Date object)
+let today = new Date(20-3-2026);
+console.log("Today's date is: " + today);
